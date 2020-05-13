61f1698264ee774ad182a24cf91fdf6d0aed0493
nmehanma <nmehanma@gmail.com>
2020-05-13 15:35:27 -0400

    add devise and associations with pic

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_211833) do
+ActiveRecord::Schema.define(version: 2020_05_13_192859) do
 
   create_table "pics", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
